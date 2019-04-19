538cad15fc4d8eb1ebad484d5748c590fcde44a1
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2019-04-19 22:18:54 +0530

    Add files via upload

---
+++ string capitalize.py
@@ -0,0 +1,23 @@
+                #difference b/w capitalize() and upper()
+# capitalize()
+a=input("Enter a string")
+capitalized_string=a.capitalize()
+print(capitalized_string)
+'''
+#output 1
+Enter a stringAkansha Bharadwaj
+Akansha bharadwaj
+#output 2
+Enter a stringakansha Bharadwaj
+Akansha bharadwaj
+'''
+#uppercase()
+a=input("Enter a string")
+capitalized_string=a.upper()
+print(capitalized_string)
+
+'''
+output
+Enter a stringakansha bharadwaj
+AKANSHA BHARADWAJ
+'''
